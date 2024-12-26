05bde0c9263792c0465627b7baf9030363468dd0
Sam Foreman <saforem2@gmail.com>
2024-12-26 12:56:10 -0600

    chore: Update `__init__.py`

---
+++ src/mmm/__init__.py
@@ -0,0 +1,15 @@
+"""
+mmm/__init__.py
+"""
+import os
+from pathlib import Path
+
+HERE = Path(os.path.abspath(__file__)).parent
+PROJECT_ROOT = HERE.parent.parent
+WORKING_DIR = Path(os.environ.get(
+    "PBS_O_WORKDIR",
+    PROJECT_ROOT,
+))
+
+OUTPUTS_DIR = WORKING_DIR.joinpath("outputs")
+OUTPUTS_DIR.mkdir(exist_ok=True, parents=True)
