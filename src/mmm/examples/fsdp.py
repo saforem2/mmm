7efc30abef5455332bba7b6dc09dc8bd92fcb2a7
Sam Foreman <saforem2@gmail.com>
2024-12-31 15:46:56 -0600

    chore: Rename `trainer/fsdp.py` -> `examples/fsdp.py`

--- src/mmm/examples/fsdp.py
+++ src/mmm/examples/fsdp.py
@@ -15,18 +15,21 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.optim.lr_scheduler import StepLR
 from torch.utils.data.distributed import DistributedSampler
+
 from torchvision import datasets, transforms
+
+from mmm import PROJECT_ROOT
 
 logger = logging.getLogger(__name__)
 
 RANK = ez.setup_torch(
-    backend=os.environ.get("BACKEND", "DDP"),
+    backend=os.environ.get('BACKEND', 'DDP'),
 )
 WORLD_SIZE = ez.get_world_size()
 DEVICE = ez.get_torch_device()
-DEVICE_ID = f"{DEVICE}:{ez.get_local_rank()}"
+DEVICE_ID = f'{DEVICE}:{ez.get_local_rank()}'
 
-logger.setLevel("INFO") if RANK == 0 else logger.setLevel("CRITICAL")
+logger.setLevel('INFO') if RANK == 0 else logger.setLevel('CRITICAL')
 
 if torch.cuda.is_available():
     torch.cuda.set_device(ez.get_local_rank())
@@ -68,7 +71,7 @@ def train(model, train_loader, optimizer, epoch, sampler=None):
         batch, target = batch.to(DEVICE_ID), target.to(DEVICE_ID)
         optimizer.zero_grad()
         output = model(batch)
-        loss = F.nll_loss(output, target, reduction="sum")
+        loss = F.nll_loss(output, target, reduction='sum')
         loss.backward()
         optimizer.step()
         ddp_loss[0] += loss.item()
@@ -77,9 +80,9 @@ def train(model, train_loader, optimizer, epoch, sampler=None):
 
     dist.all_reduce(ddp_loss, op=dist.ReduceOp.SUM)
     return {
-        "epoch": epoch,
-        "dt": t1 - t0,
-        "train_loss": ddp_loss[0] / ddp_loss[1],
+        'epoch': epoch,
+        'dt': t1 - t0,
+        'train_loss': ddp_loss[0] / ddp_loss[1],
     }
 
 
@@ -92,7 +95,7 @@ def test(model, test_loader):
             batch, target = batch.to(DEVICE_ID), target.to(DEVICE_ID)
             output = model(batch)
             ddp_loss[0] += F.nll_loss(
-                output, target, reduction="sum"
+                output, target, reduction='sum'
             ).item()  # sum up batch loss
             pred = output.argmax(
                 dim=1, keepdim=True
@@ -105,72 +108,72 @@ def test(model, test_loader):
     test_loss = ddp_loss[0] / ddp_loss[2]
 
     return {
-        "test_loss": test_loss,
-        "test_acc": 100.0 * ddp_loss[1] / ddp_loss[2],
+        'test_loss': test_loss,
+        'test_acc': 100.0 * ddp_loss[1] / ddp_loss[2],
     }
 
 
-def prepare_data() -> dict:
-    # outdir = "./data" if outdir is None else outdir
-    from pathlib import Path
-    from mmm import PROJECT_ROOT
-    outdir = Path(PROJECT_ROOT).joinpath("data")
-    outdir.mkdir(exist_ok=True, parents=True)
-    transform = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
-    )
-
-    dataset1 = datasets.MNIST(
-        outdir.as_posix(),
-        train=True,
-        download=True,
-        transform=transform,
-    )
-    torch.distributed.barrier()  # type:ignore
-    dataset2 = datasets.MNIST(outdir, train=False, transform=transform)
-
-    sampler1 = DistributedSampler(
-        dataset1, rank=RANK, num_replicas=WORLD_SIZE, shuffle=True
-    )
-    sampler2 = DistributedSampler(dataset2, rank=RANK, num_replicas=WORLD_SIZE)
-
-    train_kwargs = {"batch_size": args.batch_size, "sampler": sampler1}
-    test_kwargs = {"batch_size": args.test_batch_size, "sampler": sampler2}
-    cuda_kwargs = {"num_workers": 1, "pin_memory": True, "shuffle": False}
-    train_kwargs.update(cuda_kwargs)
-    test_kwargs.update(cuda_kwargs)
-
-    train_loader = torch.utils.data.DataLoader(  # type:ignore
-        dataset1, **train_kwargs
-    )
-    test_loader = torch.utils.data.DataLoader(  # type:ignore
-        dataset2, **test_kwargs
-    )
-    return {
-        "train": {
-            "data": dataset1,
-            "loader": train_loader,
-            "sampler": sampler1,
-        },
-        "test": {
-            "data": dataset2,
-            "loader": test_loader,
-            "sampler": sampler2,
-        },
-    }
+# def prepare_data() -> dict:
+#     # outdir = "./data" if outdir is None else outdir
+#     from pathlib import Path
+#     from mmm import PROJECT_ROOT
+#     outdir = Path(PROJECT_ROOT).joinpath("data")
+#     outdir.mkdir(exist_ok=True, parents=True)
+#     transform = transforms.Compose(
+#         [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+#     )
+#
+#     dataset1 = datasets.MNIST(
+#         outdir.as_posix(),
+#         train=True,
+#         download=True,
+#         transform=transform,
+#     )
+#     torch.distributed.barrier()  # type:ignore
+#     dataset2 = datasets.MNIST(outdir, train=False, transform=transform)
+#
+#     sampler1 = DistributedSampler(
+#         dataset1, rank=RANK, num_replicas=WORLD_SIZE, shuffle=True
+#     )
+#     sampler2 = DistributedSampler(dataset2, rank=RANK, num_replicas=WORLD_SIZE)
+#
+#     train_kwargs = {"batch_size": args.batch_size, "sampler": sampler1}
+#     test_kwargs = {"batch_size": args.test_batch_size, "sampler": sampler2}
+#     cuda_kwargs = {"num_workers": 1, "pin_memory": True, "shuffle": False}
+#     train_kwargs.update(cuda_kwargs)
+#     test_kwargs.update(cuda_kwargs)
+#
+#     train_loader = torch.utils.data.DataLoader(  # type:ignore
+#         dataset1, **train_kwargs
+#     )
+#     test_loader = torch.utils.data.DataLoader(  # type:ignore
+#         dataset2, **test_kwargs
+#     )
+#     return {
+#         "train": {
+#             "data": dataset1,
+#             "loader": train_loader,
+#             "sampler": sampler1,
+#         },
+#         "test": {
+#             "data": dataset2,
+#             "loader": test_loader,
+#             "sampler": sampler2,
+#         },
+#     }
 
 
 def prepare_model_optimizer_and_scheduler(args: argparse.Namespace) -> dict:
     model = Net().to(DEVICE_ID)
     dtypes = {
-        "fp16": torch.float16,
-        "bf16": torch.bfloat16,
-        "bfloat16": torch.bfloat16,
-        "fp32": torch.float32,
+        'fp16': torch.float16,
+        'bf16': torch.bfloat16,
+        'bfloat16': torch.bfloat16,
+        'fp32': torch.float32,
     }
     dtype = dtypes[args.dtype]
 
-    if args.dtype in {"fp16", "bf16", "fp32"}:
+    if args.dtype in {'fp16', 'bf16', 'fp32'}:
         model = FSDP(
             model,
             mixed_precision=MixedPrecision(
@@ -182,25 +185,35 @@ def prepare_model_optimizer_and_scheduler(args: argparse.Namespace) -> dict:
         model = FSDP(model)
     optimizer = optim.AdamW(model.parameters(), lr=args.lr)
 
-    logger.info(f"{model=}")
+    logger.info(f'{model=}')
 
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     return {
-        "model": model,
-        "optimizer": optimizer,
-        "scheduler": scheduler,
+        'model': model,
+        'optimizer': optimizer,
+        'scheduler': scheduler,
     }
 
 
 def fsdp_main(args: argparse.Namespace) -> None:
-    data = prepare_data()
-    train_loader = data["train"]["loader"]
-    test_loader = data["test"]["loader"]
+    # data = prepare_data()
+    from mmm.data.vision import get_mnist
+
+    data = get_mnist(
+        outdir=PROJECT_ROOT.joinpath('data', "MNIST"),
+        train_batch_size=args.batch_size,
+        test_batch_size=args.test_batch_size,
+        shuffle=False,
+        pin_memory=True,
+        num_workers=1,
+    )
+    train_loader = data['train']['loader']
+    test_loader = data['test']['loader']
 
     tmp = prepare_model_optimizer_and_scheduler(args)
-    model = tmp["model"]
-    optimizer = tmp["optimizer"]
-    scheduler = tmp["scheduler"]
+    model = tmp['model']
+    optimizer = tmp['optimizer']
+    scheduler = tmp['scheduler']
 
     history = ez.History()
     start = time.perf_counter()
@@ -210,20 +223,20 @@ def fsdp_main(args: argparse.Namespace) -> None:
             train_loader=train_loader,
             optimizer=optimizer,
             epoch=epoch,
-            sampler=data["train"]["sampler"],
+            sampler=data['train']['sampler'],
         )
         test_metrics = test(model, test_loader)
         scheduler.step()
         metrics = {**train_metrics, **test_metrics}
         _ = history.update(metrics)
         summary = ez.summarize_dict(metrics)
-        logger.info(f"{summary}")
+        logger.info(f'{summary}')
 
     logger.info(
-        " ".join(
+        ' '.join(
             [
-                f"{args.epochs + 1} epochs took",
-                f"{time.perf_counter() - start:.1f}s"
+                f'{args.epochs + 1} epochs took',
+                f'{time.perf_counter() - start:.1f}s',
             ]
         )
     )
@@ -233,82 +246,82 @@ def fsdp_main(args: argparse.Namespace) -> None:
         dist.barrier()  # wait for slowpokes
         states = model.state_dict()
         if RANK == 0:
-            torch.save(states, "mnist_cnn.pt")
+            torch.save(states, 'mnist_cnn.pt')
 
     if RANK == 0:
-        mplotdir = Path("plots")
-        tplotdir = mplotdir.joinpath("tplots")
+        mplotdir = Path('plots')
+        tplotdir = mplotdir.joinpath('tplots')
         tplotdir.mkdir(exist_ok=True, parents=True)
         dataset = history.plot_all(outdir=mplotdir)
         _ = history.tplot_all(
-            outdir=tplotdir, append=True, xkey="epoch", dataset=dataset
+            outdir=tplotdir, append=True, xkey='epoch', dataset=dataset
         )
-        logger.info(f"{dataset=}")
+        logger.info(f'{dataset=}')
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
-        description="PyTorch MNIST Example using FSDP"
+        description='PyTorch MNIST Example using FSDP'
     )
     parser.add_argument(
-        "--batch-size",
+        '--batch-size',
         type=int,
         default=64,
-        metavar="N",
-        help="input batch size for training (default: 64)",
+        metavar='N',
+        help='input batch size for training (default: 64)',
     )
     parser.add_argument(
-        "--dtype",
+        '--dtype',
         type=str,
-        default="bf16",
-        metavar="D",
-        help="Datatype for training (default=bf16).",
+        default='bf16',
+        metavar='D',
+        help='Datatype for training (default=bf16).',
     )
     parser.add_argument(
-        "--test-batch-size",
+        '--test-batch-size',
         type=int,
         default=1000,
-        metavar="N",
-        help="input batch size for testing (default: 1000)",
+        metavar='N',
+        help='input batch size for testing (default: 1000)',
     )
     parser.add_argument(
-        "--epochs",
+        '--epochs',
         type=int,
         default=10,
-        metavar="N",
-        help="number of epochs to train (default: 10)",
+        metavar='N',
+        help='number of epochs to train (default: 10)',
     )
     parser.add_argument(
-        "--lr",
+        '--lr',
         type=float,
         default=1e-3,
-        metavar="LR",
-        help="learning rate (default: 1e-3)",
+        metavar='LR',
+        help='learning rate (default: 1e-3)',
     )
     parser.add_argument(
-        "--gamma",
+        '--gamma',
         type=float,
         default=0.7,
-        metavar="M",
-        help="Learning rate step gamma (default: 0.7)",
+        metavar='M',
+        help='Learning rate step gamma (default: 0.7)',
     )
     parser.add_argument(
-        "--seed",
+        '--seed',
         type=int,
         default=1,
-        metavar="S",
-        help="random seed (default: 1)",
+        metavar='S',
+        help='random seed (default: 1)',
     )
     parser.add_argument(
-        "--save-model",
-        action="store_true",
+        '--save-model',
+        action='store_true',
         default=False,
-        help="For Saving the current Model",
+        help='For Saving the current Model',
     )
     return parser.parse_args()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     args = parse_args()
     torch.manual_seed(args.seed)
     fsdp_main(args=args)
