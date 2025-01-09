9331fdfb948915a8450f9e77b63d102c3742953f
Sam Foreman <saforem2@gmail.com>
2025-01-09 12:09:32 -0600

    feat: Add `mmm/models/llama/`

---
+++ src/mmm/models/llama/__init__.py
@@ -0,0 +1,52 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+#
+# Copyright (c) Meta Platforms, Inc. All Rights Reserved.
+"""
+mmm/models/tt/llama/__init__.py
+
+
+Modified from:
+<https://github.com/pytorch/torchtitan/blob/2a4437014e66bcf88a3f0419b816266e6326d539/torchtitan/models/llama/__init__.py>
+"""
+
+from mmm.models.llama.model import ModelArgs, Transformer
+# from torchtitan.models.llama.model import ModelArgs, Transformer
+
+__all__ = ['Transformer']
+
+llama3_configs = {
+    'debugmodel': ModelArgs(
+        dim=256, n_layers=8, n_heads=16, rope_theta=500000
+    ),
+    '8B': ModelArgs(
+        dim=4096,
+        n_layers=32,
+        n_heads=32,
+        n_kv_heads=8,
+        ffn_dim_multiplier=1.3,
+        multiple_of=1024,
+        rope_theta=500000,
+    ),
+    '70B': ModelArgs(
+        dim=8192,
+        n_layers=80,
+        n_heads=64,
+        n_kv_heads=8,
+        ffn_dim_multiplier=1.3,
+        multiple_of=4096,
+        rope_theta=500000,
+    ),
+    '405B': ModelArgs(
+        dim=16384,
+        n_layers=126,
+        n_heads=128,
+        n_kv_heads=8,
+        ffn_dim_multiplier=1.2,
+        multiple_of=4096,
+        rope_theta=500000,
+    ),
+}
