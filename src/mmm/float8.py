cd084910b7674f5a897b04e4e03ed3a127c538f1
Sam Foreman <saforem2@gmail.com>
2025-02-04 09:18:43 -0600

    chore: Format `mmm/float8.py`

--- src/mmm/float8.py
+++ src/mmm/float8.py
@@ -23,14 +23,13 @@ import torch.nn as nn
 from mmm.configs import JobConfig
 from mmm.parallelisms import ParallelDims
 
-# from torchtitan.config_manager import JobConfig
-# from torchtitan.logging import logger
-# from torchtitan.parallelisms import ParallelDims
-
 
 def _is_sm89_or_later():
     # Float8 is only supported on SM89 or later (H100+ GPUs)
-    return torch.cuda.is_available() and torch.cuda.get_device_capability() >= (8, 9)
+    return (
+        torch.cuda.is_available()
+        and torch.cuda.get_device_capability() >= (8, 9)
+    )
 
 
 class Float8Handler:
@@ -42,14 +41,18 @@ class Float8Handler:
             return
         if not _is_sm89_or_later():
             logger.warning(
-                "Failed to swap to Float8Linear because float8 is only supported on SM89 or later",
+                'Failed to swap to Float8Linear because float8 is only supported on SM89 or later',
             )
             return
         try:
-            from torchao.float8 import CastConfig, Float8LinearConfig, ScalingType
+            from torchao.float8 import (
+                CastConfig,
+                Float8LinearConfig,
+                ScalingType,
+            )
         except ImportError as e:
             raise ImportError(
-                "torchao is not installed. Please install it to use float8 linear layers."
+                'torchao is not installed. Please install it to use float8 linear layers.'
             ) from e
 
         # Mutates the model inplace replacing instances of torch.nn.Linear with Float8Linear
@@ -59,12 +62,16 @@ class Float8Handler:
         )
         scaling_type_input = ScalingType(float8_config.scaling_type_input)
         scaling_type_weight = ScalingType(float8_config.scaling_type_weight)
-        scaling_type_grad_output = ScalingType(float8_config.scaling_type_grad_output)
+        scaling_type_grad_output = ScalingType(
+            float8_config.scaling_type_grad_output
+        )
         self.config = Float8LinearConfig(
             enable_fsdp_float8_all_gather=enable_fsdp_float8_all_gather,
             cast_config_input=CastConfig(scaling_type=scaling_type_input),
             cast_config_weight=CastConfig(scaling_type=scaling_type_weight),
-            cast_config_grad_output=CastConfig(scaling_type=scaling_type_grad_output),
+            cast_config_grad_output=CastConfig(
+                scaling_type=scaling_type_grad_output
+            ),
         )
 
         self.enabled = True
@@ -84,7 +91,7 @@ class Float8Handler:
         self._sync_float8_amax_and_scale_history = None
         self.compile = job_config.training.compile
 
-        logger.info("Float8 training active")
+        logger.info('Float8 training active')
 
     def convert_to_float8_training(self, model: nn.Module):
         """
@@ -101,11 +108,11 @@ class Float8Handler:
         convert_to_float8_training(
             model,
             config=self.config,
-            module_filter_fn=lambda mod, fqn: fqn != "output",
+            module_filter_fn=lambda mod, fqn: fqn != 'output',
         )
         logger.info(
-            "Swapped to Float8Linear layers with enable_fsdp_float8_all_gather="
-            f"{self.config.enable_fsdp_float8_all_gather}"
+            'Swapped to Float8Linear layers with enable_fsdp_float8_all_gather='
+            f'{self.config.enable_fsdp_float8_all_gather}'
         )
 
     def precompute_float8_dynamic_scale_for_fsdp(
