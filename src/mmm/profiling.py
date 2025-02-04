320e524ae3999064b953efc8dac5cb2526d23cf5
Sam Foreman <saforem2@gmail.com>
2025-02-04 09:16:25 -0600

    chore: Format `mmm/profiling.py`

--- src/mmm/profiling.py
+++ src/mmm/profiling.py
@@ -10,6 +10,7 @@ mmm/profiling.py
 Modified from:
 <https://github.com/pytorch/torchtitan/blob/2a4437014e66bcf88a3f0419b816266e6326d539/torchtitan/profiling.py>
 """
+
 import contextlib
 import os
 import pickle
@@ -44,35 +45,37 @@ def maybe_enable_profiling(config: JobConfig, *, global_step: int = 0):
         rank = torch.distributed.get_rank()
 
         def trace_handler(prof):
-            curr_trace_dir_name = "iteration_" + str(prof.step_num)
+            curr_trace_dir_name = 'iteration_' + str(prof.step_num)
             curr_trace_dir = os.path.join(trace_dir, curr_trace_dir_name)
             if not os.path.exists(curr_trace_dir):
                 os.makedirs(curr_trace_dir, exist_ok=True)
 
-            logger.info(f"Dumping profiler traces at step {prof.step_num}")
+            logger.info(f'Dumping profiler traces at step {prof.step_num}')
             begin = time.monotonic()
-            prof.export_chrome_trace(f"{curr_trace_dir}/rank{rank}_trace.json")
+            prof.export_chrome_trace(f'{curr_trace_dir}/rank{rank}_trace.json')
             logger.info(
-                f"Finished dumping profiler traces in {time.monotonic() - begin:.2f} seconds"
+                f'Finished dumping profiler traces in {time.monotonic() - begin:.2f} seconds'
             )
 
-        logger.info(f"Profiling active. Traces will be saved at {trace_dir}")
+        logger.info(f'Profiling active. Traces will be saved at {trace_dir}')
 
         if not os.path.exists(trace_dir):
             os.makedirs(trace_dir, exist_ok=True)
 
         warmup, active = WARMUP, 1
         wait = profile_freq - (active + warmup)
-        assert (
-            wait >= 0
-        ), "profile_freq must be greater than or equal to warmup + active"
+        assert wait >= 0, (
+            'profile_freq must be greater than or equal to warmup + active'
+        )
         with torch.profiler.profile(
             activities=[
                 torch.profiler.ProfilerActivity.CPU,
                 torch.profiler.ProfilerActivity.CUDA,
                 torch.profiler.ProfilerActivity.XPU,
             ],
-            schedule=torch.profiler.schedule(wait=wait, warmup=warmup, active=active),
+            schedule=torch.profiler.schedule(
+                wait=wait, warmup=warmup, active=active
+            ),
             on_trace_ready=trace_handler,
             record_shapes=True,
         ) as torch_profiler:
@@ -108,25 +111,28 @@ def maybe_enable_memory_snapshot(config: JobConfig, *, global_step: int = 0):
                     return
                 if not exit_ctx:
                     curr_step = self.step_num
-                    dir_name = f"iteration_{curr_step}"
+                    dir_name = f'iteration_{curr_step}'
                 else:
                     # dump as iteration_0_exit if OOM at iter 1
                     curr_step = self.step_num - 1
-                    dir_name = f"iteration_{curr_step}_exit"
+                    dir_name = f'iteration_{curr_step}_exit'
                 curr_snapshot_dir = os.path.join(snapshot_dir, dir_name)
                 if not os.path.exists(curr_snapshot_dir):
                     os.makedirs(curr_snapshot_dir, exist_ok=True)
-                logger.info(f"Dumping memory snapshot at step {curr_step}")
+                logger.info(f'Dumping memory snapshot at step {curr_step}')
                 begin = time.monotonic()
                 with open(
-                    f"{curr_snapshot_dir}/rank{rank}_memory_snapshot.pickle", "wb"
+                    f'{curr_snapshot_dir}/rank{rank}_memory_snapshot.pickle',
+                    'wb',
                 ) as output:
                     pickle.dump(torch.cuda.memory._snapshot(), output)
                 logger.info(
-                    f"Finished dumping memory snapshot in {time.monotonic() - begin:.2f} seconds"
+                    f'Finished dumping memory snapshot in {time.monotonic() - begin:.2f} seconds'
                 )
 
-        logger.info(f"Memory profiler active. Snapshot will be saved at {snapshot_dir}")
+        logger.info(
+            f'Memory profiler active. Snapshot will be saved at {snapshot_dir}'
+        )
         profiler = MemoryProfiler(global_step, config.profiling.profile_freq)
         try:
             yield profiler
